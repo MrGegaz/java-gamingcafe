d9d9d9a351747406e5b0beb41a6d6ca7a5970ddb
MrGegaz <mrgegaz.dev@gmail.com>
2026-05-05 00:07:26 +0200

    Add DatabaseManager class & database.properties.template

---
+++ src/main/java/com/example/gamingcafe/util/DatabaseManager.java
@@ -0,0 +1,20 @@
+﻿package com.example.gamingcafe.util;
+
+import java.sql.Connection;
+
+public class DatabaseManager {
+    private static final String PROPERTIES_FILE = "database.properties";
+    private static String dbUrl;
+    private static String dbUser;
+    private static String dbPassword;
+
+    private static DatabaseManager instance;
+    private Connection connection;
+    private boolean isConnected = false;
+
+    // TODO: Finish this database manager class
+
+    private static void loadDatabaseProperties() {
+
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.gamingcafe {
     requires javafx.fxml;
 
     requires org.kordamp.ikonli.javafx;
+    requires java.sql;
 
     opens com.example.gamingcafe to javafx.fxml;
     exports com.example.gamingcafe;
